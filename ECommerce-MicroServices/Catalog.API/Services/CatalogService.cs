4a32bc4171b60838cd2f1a8792e8753f8ad1a56a
Zubair Alam <35332975+zubairalamsub@users.noreply.github.com>
2023-10-25 13:10:25 +0600

    Grpc implemented

---
+++ ECommerce-MicroServices/Catalog.API/Services/CatalogService.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.API;
+using Catalog.API.Protos;
+using Grpc.Core;
+
+namespace Catalog.API.Services
+{
+
+
+	public class CatalogService : Protos.CatalogService.CatalogServiceBase
+	{
+		private readonly ILogger<CatalogService> _logger;
+		public CatalogService(ILogger<CatalogService> logger)
+		{
+			_logger = logger;
+		}
+
+	
+		public override Task<CatalogItem> GetItemById(GetItemByIdRequest request, ServerCallContext context)
+		{
+		
+			return  Task.FromResult(new CatalogItem
+			{
+				Id = new Guid().ToString(),
+				Name = "Test",
+				Description = "Test",
+				Price = 4,
+				PictureFileName = string.Empty,
+				PictureUri = string.Empty,
+				CatalogTypeId = string.Empty,
+				CatalogBrandId = string.Empty
+			});
+		}
+
+
+	}
+}
---
+++ ECommerce-MicroServices/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Catalog.API;
+using Catalog.API.Protos;
+using Grpc.Net.Client;
+// Create a channel to the server
+var channel = GrpcChannel.ForAddress("http://localhost:5023");
+
+// Create a client with the channel
+var client = new Greeter.GreeterClient(channel);
+var client2 = new CatalogService.CatalogServiceClient(channel);
+
+
+// Call the service operations
+var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+var reply2 = client2.GetItemById(new GetItemByIdRequest { Id = "1" });
+
+
+
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Greeting: " + reply2.Name);
+Console.ReadLine();
+
+
+//// Create a client with the channel
+//var client = new CatalogService.CatalogServiceClient(channel);
+
+//// Call the service operations
+//var reply = client.GetItemById(new GetItemByIdRequest { Id = "1" });
+
+//Console.WriteLine("Greeting: " + reply.Name);
+Console.ReadLine();
